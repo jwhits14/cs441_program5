f4227202ac4280e6e01d1c4dcaf74bf9dcf0f8c3
jwhits14 <jakewhit14@gmail.com>
2021-03-22 12:50:36 -0400

    added tts object

--- app/src/main/java/com/example/program5/MainActivity.java
+++ app/src/main/java/com/example/program5/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.program5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    TextToSpeech ttobj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText write;
+        ttobj=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+            }
+        });
     }
 }
